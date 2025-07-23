ef94c5c0febe70758d4ea6f1efcb310ff1dd1c87
Ahmed Ali <a7med3li0111@gmail.com>
2025-07-23 14:22:19 +0300

    handle 1h expired validations

--- src/Routes/ProtectedRoute.jsx
+++ src/Routes/ProtectedRoute.jsx
@@ -1,16 +1,31 @@
-// ProtectedRoute.js
 import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+
+function isTokenExpired(token) {
+  try {
+    const decoded = jwtDecode(token);
+    const currentTime = Date.now() / 1000; // in seconds
+    return decoded.exp < currentTime;
+  } catch (error) {
+    return true; // If decoding fails, treat as expired
+  }
+}
 
 const ProtectedRoute = ({ children }) => {
   const auth = useSelector((state) => state.auth);
-  auth;
-
   const location = useLocation();
 
-  if (!auth.isAuthenticated) {
+  // If not authenticated or token is expired
+  const tokenExpired = isTokenExpired(auth.token);
+  if (!auth.isAuthenticated || tokenExpired) {
+    // Remove token and user from localStorage
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
     return <Navigate to="/" state={{ from: location }} replace />;
   }
+
   return children;
 };
 
